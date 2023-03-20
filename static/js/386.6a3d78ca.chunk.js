181ee0b7e0e6a1d6cf07cc4ec9dcb322a8d11f73
AllNNBusy <Butek63rus@gmail.com>
2023-03-20 05:33:35 +0300

    Updates

--- static/js/386.6a3d78ca.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcoffe_react=self.webpackChunkcoffe_react||[]).push([[386],{386:function(e,c,r){r.r(c),r.d(c,{default:function(){return a}});var f=r(184),a=function(){return(0,f.jsx)("header",{className:"our-coffee_header",children:(0,f.jsx)("div",{className:"container",children:(0,f.jsx)("h1",{className:"our-coffee_header__title fz_40",children:"Our Coffee"})})})}}}]);
-//# sourceMappingURL=386.6a3d78ca.chunk.js.map
